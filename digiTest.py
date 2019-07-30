b4aa425bd5d85cd6777df4f04417ff4fcd1cb09b
AndreasScharf <andi@r-sch.de>
2019-07-30 16:05:25 +0200

    digiTest test

--- digiTest.py
+++ digiTest.py
@@ -1,1 +1,6 @@
-f = open('/home/pi/Documents/Python/digi.txt', 'w+')
+import snap7
+
+s7 = snap7.client.Client()
+s7.connect('192.168.14.61', 0, 1)
+result = s7.db_read(101, 0.1, 0.1)
+print(get_bool(result, 0))
