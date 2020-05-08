2ca2ea51a856b3524e6be6ad1a8f0384c342b775
andreasscharf <andreas@r-sch.de>
2020-05-08 17:01:33 +0200

    bauderrate angepasst

--- digiTest.py
+++ digiTest.py
@@ -5,7 +5,7 @@ import serial
 ser = serial.Serial(
 
    port='/dev/ttyAMA0',
-   baudrate = 4800,
+   baudrate = 9600,
    parity=serial.PARITY_NONE,
    stopbits=serial.STOPBITS_ONE,
    bytesize=serial.EIGHTBITS,
