bd392c31b0006fa4a5c84c9f0c37a742ba44d727
andreasscharf <andreas@r-sch.de>
2020-05-10 21:10:34 +0200

    digitest

--- digiTest.py
+++ digiTest.py
@@ -4,17 +4,36 @@ import serial
 
 ser = serial.Serial(
 
-   port='/dev/ttyAMA0',
+   port='/dev/ttyUSB0',
    baudrate = 9600,
-   parity=serial.PARITY_NONE,
-   stopbits=serial.STOPBITS_ONE,
-   bytesize=serial.EIGHTBITS,
-   timeout=1
+   timeout=None
 )
 
+print('Port Avaliable')
+print(ser.isOpen())
+
+bytes_to_setup = [ 0x72, 0x10, 0xEE, 0x01, 0x09, 0x99, 0x45, 0x60, 0x13, 0x01, 0x94, 0x60, 0x00, 0x79, 0x01]
+bytes_to_request = [ 0x72, 0x07, 0x01, 0x00, 0x01, 0x00 ]
+
+def checksum(bytes_to_send):
+
+  sum = 0
+  for b in bytes_to_send:
+    sum = sum + b
+
+  while sum >= 0x100:
+    sum = sum - 0x100
+
+  return sum
 
 
+bytes_to_request.append(checksum(bytes_to_request))
+ser.write(bytes_to_request)
+
+time.sleep(1)
 
 while 1:
-   x = ser.read()
-   print(x)
+  if ser.inWaiting() > 0:
+    data = ser.read(size=ser.inWaiting())
+    print(data.encode("hex"))
+    print(data[-1])
