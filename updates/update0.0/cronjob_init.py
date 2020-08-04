aa026e52aee9020a589c8181e7aa317eece7d154
andreasscharf <andreas@r-sch.de>
2020-08-04 10:21:30 +0200

    jetzt läuft der cronjob

--- updates/update0.0/cronjob_init.py
+++ updates/update0.0/cronjob_init.py
@@ -1,6 +1,6 @@
 from crontab import CronTab
 cron = CronTab(user='root')
 job = cron.new(command='python /home/pi/Documents/updater.py')
-job.minute.every(0)
-job.hour.every(0)
+job.minute.on(0)
+job.hour.on(0)
 cron.write()
