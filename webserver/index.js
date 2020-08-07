098e8ff74158fd2b56606fcc126c1ea6c85d134b
andreasscharf <andreas@r-sch.de>
2020-08-07 11:45:19 +0200

    webserver fixed

--- webserver/index.js
+++ webserver/index.js
@@ -6,10 +6,9 @@ const fs = require('fs');
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 
-let assert = require('assert');
 let pythonBridge = require('python-bridge');
-const USERS_PATH = __dirname + '/users.json';
-const CONFIG_PATH = __dirname +  '/../config.json'
+const USERS_PATH = './users.json';
+const CONFIG_PATH = '../config.json'
 
 
 const VORGEGEBENE_JSON = {
@@ -18,7 +17,7 @@ const VORGEGEBENE_JSON = {
   "data": []
 }
 
-app.use(express.static(__dirname +  '/client'));
+app.use('/', express.static('client'));
 app.get('*', (req, res)=>{
     res.sendFile(__dirname + '/client/index.html');
 });
@@ -37,14 +36,16 @@ io.on('connection', function(socket){
           if(!response.data.token)
             socket.emit('login_failed', '')
           else{
+
             socket.emit('login_success', {token:response.data.token});
+            console.log('letzs hash');
             bcrypt.hash(password, 10, function(err, result) {
+              if (err)
+                console.log(err);
               let data = JSON.stringify({email: username, password: result, token:response.data.token});
               fs.writeFileSync(USERS_PATH, data);
             });
           }
-
-
         }).catch( (e) =>{
           fs.readFile(USERS_PATH, (err, data) => {
             if (err){
@@ -67,16 +68,17 @@ io.on('connection', function(socket){
 
   });
   socket.on('set_Table', (data)=>{
-    console.log('change table to', data);
+    console.log('save with out test');
+    VORGEGEBENE_JSON.data = data_table;
+    let data = JSON.stringify(VORGEGEBENE_JSON);
+    fs.writeFileSync(CONFIG_PATH, data);
+
+    /*
     let python = pythonBridge()
     python.ex`
     import snap7
     from snap7.util import *
     from snap7.snap7types import *
-    def get_dint(_bytearray, byte_index):
-        data = _bytearray[byte_index:byte_index + 4]
-        dint = struct.unpack('>i', struct.pack('4B', *data))[0]
-        return dint
 
     def checkRequest(ip, db, offset, length, datatype):
 
@@ -111,69 +113,56 @@ io.on('connection', function(socket){
         return -1
 
     `
+
     const data_table = [];
     let menge = data.table.length;
     let res_index = 0;
     for (var line of data.table) {
-      if(line.type == 'static' || line.type == 'time'){
-        line.ip = ''
-        line.db = 0
-        line.offset = 0
-        line.datatype = 0
-
-      //  data_table.push(line)
-      }
-
-      python`checkRequest(${line.ip},${line.db} , ${line.offset} , 4, ${line.datatype})`.then(x => {
-
-        if (x == 'NoConnect' || x == 'OutofRange') {
-          socket.emit('s7Error', {Error: 'NoConnect'})
-          data.table[res_index].active =   data.table[res_index].type == 'static' ||   data.table[res_index].type == 'time';
-          if (!(  data.table[res_index].type == 'static' ||   data.table[res_index].type == 'time')) {
-            data.table[res_index].not_active = true
-          }
-          data_table.push(data.table[res_index])
-          console.log('push');
-        }
-        else {
-          data.table[res_index].active = true;
-          data_table.push(data.table[res_index])
-          console.log('push');
-        }
-
+      if (line.type != 's7' || line.ip == '' || line.db == '' || line.datatype == '' || !line.active){
+        line.active = true;
+        data_table.push(line)
         res_index++;
-        console.log(res_index, menge);
-        if(menge <= res_index){
-          console.log('write');
-          VORGEGEBENE_JSON.data = data_table;
-          let mad_item = data_table.find(elem => elem.name == 'MAD')
-          VORGEGEBENE_JSON.mad = mad_item? mad_item.value: '';
-          console.log('before save',   VORGEGEBENE_JSON.data);
-          let data = JSON.stringify(VORGEGEBENE_JSON);
-          fs.writeFileSync(CONFIG_PATH, data);
-          console.log('table_change success');
-        }
-      });
+        continue
+      }else {
+        python`checkRequest(${line.ip}, ${line.db} , ${line.offset} , 4, ${line.datatype})`.then(x => {
+          console.log('line', data.table[res_index]);
+          if ((x == 'NoConnect' || x == 'OutofRange') && data.table[res_index].type == 's7') {
+            console.log(x);
+            data.table[res_index].active = false;
+            data.table[res_index].not_active = true;
+            data_table.push(data.table[res_index])
 
+            console.log('data talbe', data_table);
+            socket.emit('s7Error', {Error: 'NoConnect'})
+          }
+          else if ( data.table[res_index].type == 's7'){
+            data.table[res_index].active = true;
+            data_table.push(data.table[res_index])
+          }
+          res_index++;
+          console.log(menge, res_index);
+          if(menge <= res_index){
+            console.log('save', data_table);
+            VORGEGEBENE_JSON.data = data_table;
+            let data = JSON.stringify(VORGEGEBENE_JSON);
+            fs.writeFileSync(CONFIG_PATH, data);
+          }
+        });
+      }
     }
     python.end();
     if (menge == 0) {
+      console.log('save with out test');
       VORGEGEBENE_JSON.data = data_table;
-      let mad_item = data_table.find(elem => elem.name == 'MAD')
-      VORGEGEBENE_JSON.mad = mad_item? mad_item.value: '';
       let data = JSON.stringify(VORGEGEBENE_JSON);
       fs.writeFileSync(CONFIG_PATH, data);
-      console.log('table_change success');
     }
+    */
   });
   socket.on('get_Table', ()=>{
     fs.readFile(CONFIG_PATH, (err, data) => {
-      if(err){
-        let data = JSON.stringify(VORGEGEBENE_JSON);
-        fs.writeFileSync(CONFIG_PATH, data);
-        socket.emit('get_Table_back',{table: [] });
+      if(err)
         return
-      }
 
       let config = JSON.parse(data);
       console.log(config);
