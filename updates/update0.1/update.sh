4b91a5a077a4c4bd180db25a4ae2b9c9633685de
andreasscharf <andreas@r-sch.de>
2020-11-18 11:21:01 +0100

    ups

--- updates/update0.1/update.sh
+++ updates/update0.1/update.sh
@@ -1,3 +1,3 @@
 cp /home/pi/Documents/IotAdapter/updates/update0.1/updater.json /home/pi/Documents/updater.json
-
+python /home/pi/Documents/IotAdapter/updates/update0.1/change_to_url.py
 sudo reboot
