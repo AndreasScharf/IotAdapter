ded475989807c552e74ae5c5f4e53d58cf287990
andreasscharf <andreas@r-sch.de>
2020-11-19 11:25:51 +0100

    fehler im updater

--- updates/update0.1/update.sh
+++ updates/update0.1/update.sh
@@ -1,3 +1,7 @@
-cp /home/pi/Documents/IotAdapter/updates/update0.1/updater.json /home/pi/Documents/updater.json
+
+sudo python /home/pi/Documents/IotAdapter/updates/update0.1/cronjob_init.py
+
 python /home/pi/Documents/IotAdapter/updates/update0.1/change_to_url.py
+cp /home/pi/Documents/IotAdapter/updates/update0.1/updater.py /home/Documents/updater.py 
+cp /home/pi/Documents/IotAdapter/updates/update0.1/updater.json /home/pi/Documents/updater.json
 sudo reboot
