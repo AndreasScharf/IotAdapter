e6bde5058485f0df26b9db71dd1fe891bf5b52fa
andreascharf <>
2021-06-17 14:15:07 +0200

    update 0.4: update files

--- updates/update0.4/transform_config.py
+++
@@ -1,7 +0,0 @@
-'''
- read config file
- check if mad is == 's7300'
-
- add version manager 
- heute bis 24 uhr mit hochladen
-'''
---
+++ updates/update0.4/update.sh
@@ -0,0 +1,3 @@
+sudo pm2 restart all
+
+cp /home/pi/Documents/IotAdapter/updates/update0.4/updater.json /home/pi/Documents/updater.json
