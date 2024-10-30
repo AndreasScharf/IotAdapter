9e6adc9bf527246faca456be42aab3715256200a
andreasscharf <andreas@r-sch.de>
2024-10-30 15:02:12 +0100

    Add WEB Config Plattform for S7 PUT&GET, Network settings

--- webapi/__init__.py
+++ webapi/__init__.py
@@ -1,40 +1,126 @@
-from microdot import Microdot, send_file, Response
+from microdot import Microdot, send_file, Response, Request
 import os
+import json
+from .network import register_network_routes
+from .authendification import register_login_routes, is_session_valid
+
 
 app = Microdot()
 Response.default_content_type = 'text/html'
 
+Request.max_content_length = 1024 * 1024 * 1024
+Request.max_body_length = 16 * 1024 * 1024
+
+CONFIG_PATH = '/home/pi/Documents/IotAdapter/config.json'
+
+
 # Path to the static files directory
-STATIC_FOLDER = 'client'
-
-@app.route('/')
-async def index(request):
-    return send_file('./client/index.html')
-
-@app.route('/client/<path:path>')
-def serve_static(request, path):
-    print(path)
-    file_path = os.path.join(STATIC_FOLDER, path)
-    if os.path.isfile(file_path):
-        return Response(open(file_path, 'rb').read())
-    else:
-        return Response('File not found')
+STATIC_FOLDER = '/home/pi/Documents/IotAdapter/webapi/client'
 
 
+PORT = os.getenv('WEB_PORT', 0)
 
-@app.post('/get-data')
-def handle_post(request):
-    data = request.json
+def main(CONFIG_OBJECT):
 
+    def add_cors_headers(response):
+        # Allow requests from http://localhost:8080
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
+    @app.before_request
+    def handle_options(request):
+        
+        if request.method == 'OPTIONS':
+            response = Response()  # Create an empty response for OPTIONS
+            return add_cors_headers(response)  # Return CORS headers
+
+    @app.after_request
+    def apply_cors_headers(request, response):
+        """Apply CORS headers to all responses."""
+        return add_cors_headers(response)
+
+    @app.route('/')
+    async def index(request):
+        response = send_file(f'{STATIC_FOLDER}/index.html')
+        return add_cors_headers(response)
+
+    @app.post('/get-config')
+    def handle_post(request):
+        # authendification check
+        session_id = request.headers.get('Authorization')
+        if not (session_id and is_session_valid(session_id)):
+            response = Response({}, 403)
+            return add_cors_headers(response)
+
+
+        data = request.json
+
+        response = Response(CONFIG_OBJECT, 200)
+        return add_cors_headers(response)
+
+
+    # Handle preflight requests for CORS
+    @app.route('/set-config', methods=['POST'])
+    def handle_post(request):
+        # authendification check
+        session_id = request.headers.get('Authorization')
+        if not (session_id and is_session_valid(session_id)):
+            response = Response({}, 403)
+            return add_cors_headers(response)
+
+        data = request.json
+
+        if not 'config' in data:
+            response = Response({}, 500)
+            return add_cors_headers(response)
+        
+        config = data['config']
+        print( config['data'] )
+
+        # exchange config by reference
+        CONFIG_OBJECT['data'] = config['data']
+        
+        try:
+            # save new config file
+            with open(CONFIG_PATH, 'w', encoding='utf-8') as f:
+                    json.dump(config, f, indent=4, ensure_ascii=False)
+            print(f"Configuration saved as JSON to {CONFIG_PATH}")
+
+        except:
+            response = Response({}, 500)
+            return add_cors_headers(response)
+
+        response = Response({}, 200)
+        return add_cors_headers(response)
     
-    # Process the data here
-    return 'Data received', 200
 
-@app.post('/save-data')
-def handle_post(request):
-    data = request.json
-    # Process the data here
-    return 'Data received', 200
+    register_network_routes(app)
+    register_login_routes(app)
 
 
-app.run(port=5002)
+    @app.route('/img/<path:path>')
+    def serve_img(request, path):
+        file_path = os.path.join(STATIC_FOLDER, 'img', path)
+        if os.path.exists(file_path):
+            return send_file(file_path, content_type='image/svg+xml' if path.endswith('.svg') else None)
+
+        return Response('File not found', status=404)
+
+    # 
+    @app.route('/<path:path>')
+    def serve_static(request, path):
+        # Check if the requested file exists
+        file_path = os.path.join(STATIC_FOLDER, path)
+        if os.path.exists(file_path):
+            return send_file(file_path)
+
+        # Serve nindex.html for unmatched paths (for Vue Router)
+        return send_file(os.path.join(STATIC_FOLDER, 'index.html'))
+
+
+    if PORT:
+        app.run(debug=True, port=PORT)
+
+#asyncio.run(main())
---
+++ webapi/authendification.py
@@ -0,0 +1,88 @@
+from microdot import Response
+from crypt import crypt
+import uuid
+import time
+import subprocess
+
+
+# In-memory session store
+sessions = {}
+
+def create_session():
+    session_id = str(uuid.uuid4())
+
+    expiration_time = time.time() + 3600  # 1-hour expiration from now
+    sessions[session_id] = expiration_time
+
+    return session_id
+
+def is_session_valid(session_id):
+    current_time = time.time()
+    expiration_time = sessions.get(session_id)
+    
+    # Check if session exists and if it is still valid
+    if expiration_time and current_time < expiration_time:
+        return True
+    else:
+        # If session has expired, remove it
+        if session_id in sessions:
+            del sessions[session_id]
+        return False
+
+
+
+def read_shadow():
+    try:
+        # Run the cat command on /etc/shadow with sudo
+        result = subprocess.run(
+            ['sudo', 'cat', '/etc/shadow'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        # Print the output
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Error reading /etc/shadow: {e.stderr}")
+
+
+# Helper function to get the hashed password from /etc/shadow for a user
+def get_user_hash(username):
+
+    shadow_file = read_shadow()
+    for line in shadow_file.split('\n'):
+        fields = line.strip().split(":")
+        if fields[0] == username:
+            return fields[1]
+        
+
+    return None
+
+# Verify user credentials
+def verify_login(username, password):
+    stored_hash = get_user_hash(username)
+    if not stored_hash:
+        return False
+    
+    # Extract salt from the stored hash
+    salt = "$".join(stored_hash.split("$")[:3])
+    # Hash the provided password with the salt
+    hashed_password = crypt(password, salt)
+    
+    return hashed_password == stored_hash
+
+def register_login_routes(app):
+    # Web route for login page
+    @app.route('/login', methods=['POST'])
+    def login(request):
+        data = request.json
+
+        username = data['username'] if 'username' in data else False
+        password = data['password'] if 'password' in data else False
+        
+
+        if username and password and verify_login(username, password):
+            return Response({ "session_id": create_session() }, 200)
+        else:
+            return Response({ "error": "Login Failed" }, 401)
+        
